91d5c76c15b2b0a031143a332658e2dccfdc6a80
Iago Veras <ivdm0406@gmail.com>
2026-01-26 22:38:10 -0300

    feat: add ticket update route and normalize ticket routes

--- backend/routes/ticket.php
+++ backend/routes/ticket.php
@@ -5,8 +5,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->group(function () {
 
-    Route::prefix('tickets/')->group(function (){
+    Route::prefix('tickets')->group(function () {
         Route::get('/', [TicketController::class, 'index'])->name('tickets.name');
-        Route::post('create/', [TicketController::class, 'store'])->name('tickets.store');
+        Route::post('/create', [TicketController::class, 'store'])->name('tickets.store');
+
+        Route::prefix('/{uuid}')->group(function () {
+            Route::put('/update', [TicketController::class, 'update'])->name('tickets.update');
+        });
     });
 });
